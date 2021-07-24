73cdfba0311d06beaa0c8bdc946d6be17c84c044
Mamta <mamtarajput925@gmail.com>
2021-07-24 18:41:10 +0530

    add suthentication

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './dto/jwt-payload.inteface.dto';
+import { User } from './user.entity';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+  ) {
+    super({
+      secretOrKey: 'topSecret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const { username } = payload;
+    const user: User = await this.usersRepository.findOne({ username });
+    if (!user) {
+      return new UnauthorizedException();
+    }
+    return user;
+  }
+}
